20acba5f9d2cc16ff6424a6f5042e21d6fdacaef
sincevoid <oficial.loststudios@gmail.com>
2023-04-29 22:36:08 -0300

    First Commit

---
+++ src/ReplicatedStorage/Util.lua
@@ -0,0 +1,8 @@
+local Bindables = {
+    ["ProfileLoad"] = Instance.new("BindableEvent"),
+}
+
+local Util = {}
+Util.Bindables = Bindables
+
+return Util
---
+++ src/ServerScriptService/Server/init.server.lua
@@ -0,0 +1,46 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+-- # Modules
+local ProfileService = require(ServerScriptService:WaitForChild("ProfileService"))
+local Util = require(ReplicatedStorage:WaitForChild("Util"))
+
+local Profiles = require(ReplicatedStorage:WaitForChild("Profiles"))
+local ProfileStore = ProfileService.GetProfileStore("Player_Data", {})
+
+local Storage = {}
+local Bindables = Util.Bindables
+
+local function PlayerAdded(player: Player)
+    local profile = ProfileStore:LoadProfileAsync("Player_" .. player.UserId)
+    if profile ~= nil then
+        profile:AddUserId(player.UserId) -- GDPR compliance
+        profile:Reconcile() -- Fill in missing variables from ProfileTemplate (optional)
+        profile:ListenToRelease(function()
+            Storage[player] = nil
+            Profiles[player] = nil
+            player:Kick()
+        end)
+        if player:IsDescendantOf(Players) == true then
+            Storage[player] = profile
+            Profiles[player] = profile.Data
+            Bindables.ProfileLoad:Fire(player, profile.Data)
+        else
+            profile:Release()
+        end
+    else
+        player:Kick()
+    end
+
+end
+
+local function PlayerLeft(Player: Player)
+    Profiles[Player] = nil
+    if Storage[Player] ~= nil then
+        Storage[Player]:Release()
+    end
+end
+
+Players.PlayerAdded:Connect(PlayerAdded)
+Players.PlayerDisconnecting:Connect(PlayerLeft)
